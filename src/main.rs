004b00e208f14f12c144589291d46bdcbcb03369
Emiliano Dalla Verde Marcozzi <6564766d+git@gmail.com>
2024-11-08 21:38:50 -0500

    Add colors

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 use clap::Parser;
+use colored::Colorize;
 
 #[derive(Parser)]
 struct Options {
     #[clap(default_value = "Meow!")]
     /// What does the cat say?
-    message: String
+    message: String,
+
+    #[clap(short = 'd', long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
 }
 
 
 fn main() {
     let options = Options::parse();
     let message = options.message;
-    println!("{}", message);
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("Cats do not bark!");
+        std::process::exit(1);
+    }
+
+    let eye = if options.dead { "x" } else { "0" };
+
+    println!("{}", message.bright_yellow().underline().on_purple());
     println!(" \\");
     println!("  \\");
     println!("   \\");
     println!("    /\\_/\\");
-    println!("   ( o.o )");
+    println!("   ( {eye}.{eye} )", eye=eye.red().bold());
     println!("    > ^ <");
     println!("   /  -  \\");
     println!("  /        \\/");
